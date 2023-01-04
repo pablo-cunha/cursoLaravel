d5b43c49d68d533c8c3ea849706ccd72cde0cd83
pablo-cunha <lcunha.pablo@gmail.com>
2023-01-04 16:35:08 -0300

    Rotas e Views

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
+Route::get('/product', [\App\Http\Controllers\ProductController::class, 'show']);
+
+// Admin
+Route::get('/admin/products', [\App\Http\Controllers\AdminProductsController::class,  'show']);
+Route::get('/admin/add-product', [\App\Http\Controllers\AdminAddProductController::class, 'show']);
